685a73a7aae6a5f8066d589ef1753717d5f65f29
Reyes4711 <carlos.rg.92.47@gmail.com>
2021-05-14 12:38:48 +0200

    [14.0][MIG] product_analytic_purchase: Migration to 14.0

--- product_analytic_purchase/__manifest__.py
+++ product_analytic_purchase/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2017 Akretion (http://www.akretion.com/) - Alexis de Lattre
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Product Analytic Purchase",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Purchases",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": "Glue module between purchase and product_analytic",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
